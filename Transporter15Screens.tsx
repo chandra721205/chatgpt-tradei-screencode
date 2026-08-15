154847c8450e54ee3f28319240943be8350f902d
chandra721205 <86641544+chandra721205@users.noreply.github.com>
2026-08-15 12:19:28 +0530

    Add transporter screen module

---
+++ Transporter15Screens.tsx
@@ -0,0 +1,3 @@
+// Port of the transporter screen module from chandra721205/trade-figma-old.
+// Source branch: feat/figma-15-transporter-module
+export { default as Transporter15Screens } from './Transporter15Screens';
