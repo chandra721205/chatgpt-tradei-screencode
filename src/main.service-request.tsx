587227f158d8c67229a1e558a35939e4c2eb3f1a
chandra721205 <86641544+chandra721205@users.noreply.github.com>
2026-08-15 13:03:17 +0530

    Add standalone service-request entry point

---
+++ src/main.service-request.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ServiceRequests10ScreenRouter from "./services/ServiceRequests10ScreenRouter";
+import "./styles/index.css";
+
+createRoot(document.getElementById("root")!).render(<ServiceRequests10ScreenRouter />);
