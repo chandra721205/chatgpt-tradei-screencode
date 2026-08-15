8e151d8bd8d1655c96d6f9b64a26360a47a14171
chandra721205 <86641544+chandra721205@users.noreply.github.com>
2026-08-15 13:03:03 +0530

    Add service-request screen router

---
+++ services/ServiceRequests10ScreenRouter.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ServiceRequests10Screens from "./ServiceRequests10Screens";
+
+export default function ServiceRequests10ScreenRouter(){
+  const route=window.location.hash.replace(/^#/,"");
+  const screen=route.startsWith("svc-")?route.slice(4):"marketplace";
+  return <ServiceRequests10Screens screen={screen}/>;
+}
